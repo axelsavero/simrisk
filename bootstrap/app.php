0f98bd0fc6455aa04ec72adb53130714f19b60b8
Foxie735 <muhammadaxel139@gmail.com>
2025-09-23 11:04:56 +0700

    ngebenerin masalah 419 page expired di laporan

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->validateCsrfTokens(
+        except: ['laporan/export-pdf', 'laporan/export-excel']
+    );
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
